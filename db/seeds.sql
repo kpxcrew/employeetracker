0711669efcc8da9484e53a200fc253af8c5f3eb0
kpxcrew <118790881+kpxcrew@users.noreply.github.com>
2023-04-10 23:56:35 -0500

    added seeds data

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (name)
+VALUES  ('Human Resources'), 
+        ('Accounting'), 
+        ('Legal'), 
+        ('Sales');
+
+INSERT INTO
+    role (title, salary, department_id)
+VALUES  ('Sales Lead', 120000, 4), 
+        ('Salesperson', 65000, 4), 
+        ('Human Resource Generalist', 50000, 1), 
+        ('Lead Human Resource Administrator', 80000, 1), 
+        ('Payroll Coordinator', 50000, 1),
+        ('Account Manager', 165000, 2), 
+        ('Accountant', 130000, 2), 
+        ('Paralegal', 75000, 3), 
+        ('Records Coordinator', 43000, 3),
+        ('Lawyer', 210000, 3);
+
+INSERT INTO
+    employee (
+        first_name,
+        last_name,
+        role_id,
+        manager_id
+    )
+VALUES  ('Stoop', 'Kid', 1, null), 
+        ('Norbert', 'Beaver', 2, 1), 
+        ('Patti', 'Mayonnaise', 3, null), 
+        ('Chuckie', 'Finster', 4, 3), 
+        ('Squidward', 'Tentacles', 5, null), 
+        ('Tommy', 'Pickles', 6, 5), 
+        ('Nigel', 'Thornberry', 7, null), 
+        ('Doug', 'Funnie', 8, 7);
